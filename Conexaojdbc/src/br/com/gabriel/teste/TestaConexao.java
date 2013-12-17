2309e6c0b41326fd7cecf10a03e45bd16c0d6c29
gabriel <gabriel@k9>
2013-12-17 01:43:03 -0200

    getter e setters

--- Conexaojdbc/src/br/com/gabriel/teste/TestaConexao.java
+++ Conexaojdbc/src/br/com/gabriel/teste/TestaConexao.java
@@ -1,9 +1,21 @@
 package br.com.gabriel.teste;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.gabriel.connectionfactory.FabricaDeConexoes;
+
 public class TestaConexao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Connection c = new FabricaDeConexoes().getConexao();
+		System.out.println("Conectou!");
+		try {
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
