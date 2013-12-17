7337aed36340af547d54e9292e645e08f8d9807f
gabriel <gabriel@k9>
2013-12-16 22:06:03 -0200

    Identação!

--- Conexaojdbc/src/br/com/gabriel/jdbc/JdbcExemplo.java
+++ Conexaojdbc/src/br/com/gabriel/jdbc/JdbcExemplo.java
@@ -6,15 +6,15 @@ import java.sql.SQLException;
 
 public class JdbcExemplo {
 
-	public static void main(String[] args){
-  
-	try {
-		Connection conexao = DriverManager.getConnection("jdbc:mysql://127.0.0.1/test","root","cancer");
-		System.out.println("Conectado com sucesso..!");
-        conexao.close();
-	} catch (SQLException e) {
-		e.printStackTrace();
-	}	
-    }
-	}
+	public static void main(String[] args) {
 
+		try {
+			Connection conexao = DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1/test", "root", "cancer");
+			System.out.println("Conectado com sucesso..!");
+			conexao.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
